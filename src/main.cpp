e8abd4c21e40af2d63fc64f937d1edb260ac0369
Jakub Turek <turek.jk@gmail.com>
2021-03-24 22:35:17 +0100

    basic function

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "microMouse.h"
-#include "microMouse.cpp"
+//#include "microMouse.cpp"
 
 using namespace std;
 
 int main(){
-    
+    double a = 2.1;
+    MicroMouse *ms = new MicroMouse(a);
+    cout << ms->get_dimension_x();
     return 0;
 }
--- src/microMouse.h
+++ src/microMouse.h
@@ -1,6 +1,6 @@
 #ifndef MICRO_MOUSE
 #define MICRO_MOUSE
- 
+
 class MicroMouse {
     private:
         double dimension_x;
@@ -14,14 +14,16 @@ class MicroMouse {
         void set_dimension_x(double dimension_x);
         double get_dimension_y();
         void set_dimension_y(double dimension_y);
+        double get_motor_speed();    
         void set_motor_speed(double motor_speed);
-        double get_motor_speed();
         double get_rotation_speed();
         void set_rotation_speed(double rotation_speed);
 
         // TODO
         void scan_surroundings();
         void go_to_exit();
+
+        MicroMouse();
 
         MicroMouse(double dimension_x);
 
